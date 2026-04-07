491a4364aebdd5a614bd34406674b607f3f894cb
mooncake202 <142457189+mooncake202@users.noreply.github.com>
2026-04-07 14:06:21 -0600

    quite baja temporal

---
+++ prisma/migrations/20260407200557_remove_baja_temporal_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `bajaTemporal` on the `alumno` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `alumno` DROP COLUMN `bajaTemporal`;
