6e1cc95669092ed18c303475b1ba87ab3f541051
mooncake202 <karenlagarza02@gmail.com>
2026-07-07 13:09:08 -0600

    bd del diagrama conectada creo

--- prisma/migrations/20260402211213_quitar_defaults_cubicul_semestre/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `alumno` ALTER COLUMN `semestre` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `profesor` ALTER COLUMN `cubiculo` DROP DEFAULT;
--- prisma/migrations/20260407200557_remove_baja_temporal_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `bajaTemporal` on the `alumno` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `alumno` DROP COLUMN `bajaTemporal`;
